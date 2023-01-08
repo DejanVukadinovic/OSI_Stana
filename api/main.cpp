1b014e505b0f29511bd0246e3e561ba6b0540772
Maja Tripunovic <tripunovicmaja@gmail.com>
2023-01-08 22:40:56 +0100

    bus class functionality

--- api/main.cpp
+++ api/main.cpp
@@ -12,13 +12,13 @@
 
 #include "string"
 
-crow::json::wvalue list_users()
+crow::json::wvalue bus_class(const std::string description,double price_coefficient)
 {
 	try
 	{
 		sql::Driver* driver;
 		sql::Connection* con;
-		sql::Statement* stmt;
+		sql::PreparedStatement* stmt;
 		sql::ResultSet* res;
 		crow::json::wvalue result;
 
@@ -27,21 +27,19 @@ crow::json::wvalue list_users()
 		std::cout<<"Connected"<<std::endl;
 		con->setSchema(getenv("DB_NAME"));
 
-		stmt = con->createStatement();
-		res = stmt->executeQuery("SELECT * FROM user");
-		while (res->next()) {
-			crow::json::wvalue::object tmp;
-			tmp["username"] = res->getString("username");
-			tmp["deleted"] = res->getInt("deleted")?"True":"False";
-			tmp["name"] = res->getString("name");
-			
-			result[res->getString("iduser")] = tmp;
-		}
-
+		stmt = con->prepareStatement("INSERT INTO bus_class(description,price_coefficient,deleted) VALUES(?,?,?)");
+		stmt->setString(1, description);
+		stmt->setDouble(2,price_coefficient) ;
+		stmt->setInt(3, 0);
+		res=stmt->executeQuery();
+		std::string message="Bus class added";
+            result["Message"]=message;
+		
 		delete res;
 		delete stmt;
 		delete con;
 		return result;
+
 
 	}
 	catch (sql::SQLException& e)										
@@ -64,10 +62,16 @@ int main()
 		{
 			return "Hello world!!";
 		});
-	CROW_ROUTE(app, "/list")([]()
+	CROW_ROUTE(app, "/bus_class")([](const crow::request& req)
 		{
-			crow::json::wvalue res = list_users();
-			return list_users();
+			std::string body = req.body;
+			std::cout<<body<<std::endl;
+			std::string first = body.substr(body.find("\n")+1, body.find(";"));
+			const std::string description = req.get_header_value("description");
+			const std::string price_coefficient_s = req.get_header_value("price_coefficient_s");
+			double price_coefficient= std::stod(price_coefficient_s);
+			crow::json::wvalue result = bus_class(description,price_coefficient);
+			return result;
 		});
 	std::cout<<"Running on: http://120.0.0.1:3001"<<std::endl;
 	app.port(params::port).run();
