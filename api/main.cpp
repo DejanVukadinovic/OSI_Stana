3fe41502d1d3110724da46053062215e36cb481e
DESKTOP-SI8Q6B0\HP <srdjan.zeljkovic1@gmail.com>
2023-01-11 16:45:05 +0100

    Added set_route_driver functionality

--- api/main.cpp
+++ api/main.cpp
@@ -100,6 +100,47 @@ crow::json::wvalue discount(const int min_age,const int max_age,const double coe
 	}		
 }
 
+crow::json::wvalue set_route_driver(const int iddriver, const int idroute)
+{
+    try
+    {
+        sql::Driver* driver;
+        sql::Connection* con;
+        sql::PreparedStatement* stmt;
+        sql::ResultSet* res;
+        crow::json::wvalue result;
+
+        driver = get_driver_instance();
+        con = driver->connect(getenv("DB_HOST"), getenv("DB_USER"), getenv("DB_PASSWORD"));
+        std::cout<<"Connected"<<std::endl;
+        con->setSchema(getenv("DB_NAME"));
+
+        stmt = con->prepareStatement("INSERT INTO route_has_driver (iddriver, idroute) VALUES (?, ?)");
+        stmt->setInt(1, iddriver);
+        stmt->setInt(2, idroute);
+        res=stmt->executeQuery();
+
+        std::string message="Route and driver set successfully";
+        result["Message"] = message;
+
+		delete res;
+        delete stmt;
+        delete con;
+        return result;
+    }
+    catch (sql::SQLException& e)
+    {
+        std::cout << "# ERR: SQLException in " << __FILE__;
+        std::cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << std::endl;
+        std::cout << "# ERR: " << e.what();
+        std::cout << " (MySQL error code: " << e.getErrorCode();
+        std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
+        crow::json::wvalue ret;
+        ret["ERROR:"] = e.what();
+        return  ret;
+    }
+}
+
 int main()
 {
 	crow::SimpleApp app;
@@ -130,6 +171,19 @@ int main()
 			const std::string coefficient_s = req.get_header_value("coefficient");
 			const double coefficient= std::stod(coefficient_s);
 			crow::json::wvalue result = discount(min_age,max_age,coefficient);
+			return result;
+			
+		});
+		CROW_ROUTE(app, "/set-route-driver")([](const crow::request& req)
+		{
+			std::string body = req.body;
+			std::cout<<body<<std::endl;
+			std::string first = body.substr(body.find("\n")+1, body.find(";"));
+			const std::string iddriver_s = req.get_header_value("iddriver");
+			const int driver_a=std::stoi(iddriver_s);
+			const std::string idroute_s = req.get_header_value("idroute");
+			const int route_a=std::stoi(idroute_s);
+			crow::json::wvalue result = set_route_driver(driver_a,route_a);
 			return result;
 			
 		});
